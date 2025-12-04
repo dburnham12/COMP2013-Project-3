56925c19fce8871b168505e4f9c3dc80d01af3d1
Tania H <tania.horvat@student.sl.on.ca>
2025-12-04 16:43:15 -0500

    Added Add Product Page, incorporated valid ADMIN_NAMES into NavBar and Add Product Page

--- frontend/src/Components/GroceriesAppContainer.jsx
+++ frontend/src/Components/GroceriesAppContainer.jsx
@@ -1,228 +1,185 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import CartContainer from "./CartContainer";
 import ProductsContainer from "./ProductsContainer";
 import NavBar from "./NavBar";
 import axios from "axios";
-import ProductForm from "./ProductForm";
+import { jwtDecode } from "jwt-decode";
+import Cookies from "js-cookie";
+import { ADMIN_NAMES } from "../utility/constants";
 
 export default function GroceriesAppContainer() {
-  /////////// States ///////////
-  const [productQuantity, setProductQuantity] = useState();
-  const [cartList, setCartList] = useState([]);
-  const [productList, setProductList] = useState([]);
-  const [postResponse, setPostResponse] = useState("");
-  const [formData, setFormData] = useState({
-    productName: "",
-    brand: "",
-    image: "",
-    price: "",
-  });
-  const [isEditing, setIsEditing] = useState(false);
-
-  //////////useEffect////////
-
-  useEffect(() => {
-    handleProductsFromDB();
-  }, [postResponse]);
-
-  ////////Handlers//////////
-  const initialProductQuantity = (prods) =>
-    prods.map((prod) => {
-      return { id: prod.id, quantity: 0 };
+    /////////// States ///////////
+    const [productQuantity, setProductQuantity] = useState();
+    const [cartList, setCartList] = useState([]);
+    const [productList, setProductList] = useState([]);
+    const [postResponse, setPostResponse] = useState("");
+    const [currentUser, setCurrentUser] = useState(() => {
+        const jwtToken = Cookies.get("jwt-authorization");
+        if (!jwtToken) {
+            return "";
+        }
+        try {
+            const decodedToken = jwtDecode(jwtToken);
+            return decodedToken.username;
+        } catch {
+            return "";
+        }
     });
 
-  const handleProductsFromDB = async () => {
-    try {
-      await axios.get("http://localhost:3000/products").then((result) => {
-        setProductList(result.data);
-        setProductQuantity(initialProductQuantity(result.data));
-      });
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+    const navigate = useNavigate();
 
-  const handleOnChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    //////////useEffect////////
 
-  const handleOnSubmit = async (e) => {
-    if (isEditing) {
-      e.preventDefault();
-      handleUpdateProduct(formData._id);
-      setIsEditing(false);
-      setFormData({
-        productName: "",
-        brand: "",
-        image: "",
-        price: "",
-      });
-    } else {
-      e.preventDefault();
-      try {
-        await axios
-          .post("http://localhost:3000/add-product", formData)
-          .then((result) => {
-            setPostResponse(result.data);
-          });
-        setFormData({
-          productName: "",
-          brand: "",
-          image: "",
-          price: "",
+    useEffect(() => {
+        handleProductsFromDB();
+    }, [postResponse]);
+
+    ////////Handlers//////////
+    const initialProductQuantity = (prods) =>
+        prods.map((prod) => {
+            return { id: prod.id, quantity: 0 };
         });
-      } catch (error) {
-        console.log(error.message);
-      }
-    }
-  };
 
-  const handleEditProduct = (product) => {
-    setFormData({
-      productName: product.productName,
-      brand: product.brand,
-      image: product.image,
-      price: product.price,
-      _id: product._id,
-    });
-    setIsEditing(true);
-    setPostResponse("");
-  };
-
-  const handleUpdateProduct = async (productId) => {
-    try {
-      await axios
-        .patch(`http://localhost:3000/products/${productId}`, formData)
-        .then((result) => {
-          setPostResponse(result.data);
-        });
-      setFormData({
-        productName: "",
-        brand: "",
-        image: "",
-        price: "",
-      });
-      setIsEditing(false);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const handleAddQuantity = (productId, mode) => {
-    if (mode === "cart") {
-      const newCartList = cartList.map((product) => {
-        if (product.id === productId) {
-          return { ...product, quantity: product.quantity + 1 };
+    const handleProductsFromDB = async () => {
+        try {
+            await axios.get("http://localhost:3000/products").then((result) => {
+                setProductList(result.data);
+                setProductQuantity(initialProductQuantity(result.data));
+            });
+        } catch (error) {
+            console.log(error.message);
         }
-        return product;
-      });
-      setCartList(newCartList);
-      return;
-    } else if (mode === "product") {
-      const newProductQuantity = productQuantity.map((product) => {
-        if (product.id === productId) {
-          return { ...product, quantity: product.quantity + 1 };
-        }
-        return product;
-      });
-      setProductQuantity(newProductQuantity);
-      return;
-    }
-  };
+    };
 
-  const handleRemoveQuantity = (productId, mode) => {
-    if (mode === "cart") {
-      const newCartList = cartList.map((product) => {
-        if (product.id === productId && product.quantity > 1) {
-          return { ...product, quantity: product.quantity - 1 };
-        }
-        return product;
-      });
-      setCartList(newCartList);
-      return;
-    } else if (mode === "product") {
-      const newProductQuantity = productQuantity.map((product) => {
-        if (product.id === productId && product.quantity > 0) {
-          return { ...product, quantity: product.quantity - 1 };
-        }
-        return product;
-      });
-      setProductQuantity(newProductQuantity);
-      return;
-    }
-  };
+    const handleEditProduct = (product) => {
+        // setFormData({
+        //     productName: product.productName,
+        //     brand: product.brand,
+        //     image: product.image,
+        //     price: product.price,
+        //     _id: product._id,
+        // });
+        // setIsEditing(true);
+        // setPostResponse("");
+    };
 
-  const handleDeleteProduct = async (productId) => {
-    try {
-      await axios
-        .delete(`http://localhost:3000/products/${productId}`)
-        .then((result) => {
-          console.log(result);
-          setPostResponse(
-            `${result.data.productName} deleted\n with id: ${result.data.id}`
-          );
-        });
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+    const handleAddQuantity = (productId, mode) => {
+        if (mode === "cart") {
+            const newCartList = cartList.map((product) => {
+                if (product.id === productId) {
+                    return { ...product, quantity: product.quantity + 1 };
+                }
+                return product;
+            });
+            setCartList(newCartList);
+            return;
+        } else if (mode === "product") {
+            const newProductQuantity = productQuantity.map((product) => {
+                if (product.id === productId) {
+                    return { ...product, quantity: product.quantity + 1 };
+                }
+                return product;
+            });
+            setProductQuantity(newProductQuantity);
+            return;
+        }
+    };
 
-  const handleAddToCart = (productId) => {
-    const product = productList.find((product) => product.id === productId);
-    const pQuantity = productQuantity.find(
-      (product) => product.id === productId
+    const handleRemoveQuantity = (productId, mode) => {
+        if (mode === "cart") {
+            const newCartList = cartList.map((product) => {
+                if (product.id === productId && product.quantity > 1) {
+                    return { ...product, quantity: product.quantity - 1 };
+                }
+                return product;
+            });
+            setCartList(newCartList);
+            return;
+        } else if (mode === "product") {
+            const newProductQuantity = productQuantity.map((product) => {
+                if (product.id === productId && product.quantity > 0) {
+                    return { ...product, quantity: product.quantity - 1 };
+                }
+                return product;
+            });
+            setProductQuantity(newProductQuantity);
+            return;
+        }
+    };
+
+    const handleDeleteProduct = async (productId) => {
+        try {
+            await axios
+                .delete(`http://localhost:3000/products/${productId}`)
+                .then((result) => {
+                    console.log(result);
+                    setPostResponse(
+                        `${result.data.productName} deleted\n with id: ${result.data.id}`
+                    );
+                });
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    const handleAddToCart = (productId) => {
+        const product = productList.find((product) => product.id === productId);
+        const pQuantity = productQuantity.find(
+            (product) => product.id === productId
+        );
+        const newCartList = [...cartList];
+        const productInCart = newCartList.find(
+            (product) => product.id === productId
+        );
+        if (productInCart) {
+            productInCart.quantity += pQuantity.quantity;
+        } else if (pQuantity.quantity === 0) {
+            alert(`Please select quantity for ${product.productName}`);
+        } else {
+            newCartList.push({ ...product, quantity: pQuantity.quantity });
+        }
+        setCartList(newCartList);
+    };
+
+    const handleRemoveFromCart = (productId) => {
+        const newCartList = cartList.filter(
+            (product) => product.id !== productId
+        );
+        setCartList(newCartList);
+    };
+
+    const handleClearCart = () => {
+        setCartList([]);
+    };
+
+    /////////Renderer
+    return (
+        <div>
+            <NavBar
+                quantity={cartList.length}
+                navigate={navigate}
+                currentUser={currentUser}
+                adminNames={ADMIN_NAMES}
+            />
+            <div className="GroceriesApp-Container">
+                <ProductsContainer
+                    products={productList}
+                    handleAddQuantity={handleAddQuantity}
+                    handleRemoveQuantity={handleRemoveQuantity}
+                    handleAddToCart={handleAddToCart}
+                    productQuantity={productQuantity}
+                    handleEditProduct={handleEditProduct}
+                    handleDeleteProduct={handleDeleteProduct}
+                />
+                <CartContainer
+                    cartList={cartList}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                    handleAddQuantity={handleAddQuantity}
+                    handleRemoveQuantity={handleRemoveQuantity}
+                    handleClearCart={handleClearCart}
+                />
+            </div>
+        </div>
     );
-    const newCartList = [...cartList];
-    const productInCart = newCartList.find(
-      (product) => product.id === productId
-    );
-    if (productInCart) {
-      productInCart.quantity += pQuantity.quantity;
-    } else if (pQuantity.quantity === 0) {
-      alert(`Please select quantity for ${product.productName}`);
-    } else {
-      newCartList.push({ ...product, quantity: pQuantity.quantity });
-    }
-    setCartList(newCartList);
-  };
-
-  const handleRemoveFromCart = (productId) => {
-    const newCartList = cartList.filter((product) => product.id !== productId);
-    setCartList(newCartList);
-  };
-
-  const handleClearCart = () => {
-    setCartList([]);
-  };
-  /////////Renderer
-  return (
-    <div>
-      <NavBar quantity={cartList.length} />
-      <div className="GroceriesApp-Container">
-        <ProductForm
-          handleOnSubmit={handleOnSubmit}
-          postResponse={postResponse}
-          handleOnChange={handleOnChange}
-          formData={formData}
-          isEditing={isEditing}
-        />
-        <ProductsContainer
-          products={productList}
-          handleAddQuantity={handleAddQuantity}
-          handleRemoveQuantity={handleRemoveQuantity}
-          handleAddToCart={handleAddToCart}
-          productQuantity={productQuantity}
-          handleEditProduct={handleEditProduct}
-          handleDeleteProduct={handleDeleteProduct}
-        />
-        <CartContainer
-          cartList={cartList}
-          handleRemoveFromCart={handleRemoveFromCart}
-          handleAddQuantity={handleAddQuantity}
-          handleRemoveQuantity={handleRemoveQuantity}
-          handleClearCart={handleClearCart}
-        />
-      </div>
-    </div>
-  );
 }
